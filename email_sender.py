356736be57565002d62ee6a357009462d9ae22e5
erannir1 <erannir94@gmail.com>
2021-06-15 16:35:45 +0300

    Change hardcoded to import from parameters

--- email_sender.py
+++ email_sender.py
@@ -7,23 +7,25 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
+from parameters import sent_email_addresses_path, search_words, group_id, mail_subject, mail_attach, mail_content_path,\
+    number_of_page_to_harvest
 
 
-def send_resume_to_rec(emails_list, grade, mail_subject, mail_content, attachment):
+def send_resume_to_rec(emails_list, mail_grade, mail_subject_line, email_content, attachment):
     sent_email_addresses = []
     # The mail addresses and password
     sender_address = gmail_username
     sender_pass = gmail_password
     for receiver in emails_list:
-        if receiver[1] >= grade and receiver[0] not in sent_email_addresses:
+        if receiver[1] >= mail_grade and receiver[0] not in sent_email_addresses:
             # Setup the MIME
             message = MIMEMultipart()
             message['From'] = "Eran Nir <{}>".format(sender_address)
             message['To'] = receiver[0]
-            message['Subject'] = mail_subject  # The subject line
+            message['Subject'] = mail_subject_line  # The subject line
             # The body and the attachments for the mail
 
-            message.attach(MIMEText(mail_content, 'plain'))
+            message.attach(MIMEText(email_content, 'plain'))
             payload = MIMEBase('application', "octet-stream")
             with open(attachment, 'rb') as attach_file:  # Open the file as binary mode
                 payload.set_payload(attach_file.read())
@@ -48,21 +50,16 @@ def send_resume_to_rec(emails_list, grade, mail_subject, mail_content, attachmen
 
 def upload_list_pickle(list_to_upload, path, upload=False):
     if upload:
-        with open(sent_email_addresses_path, "wb") as seap:  # Pickling
-            pickle.dump(sent_email_addresses, seap)
+        with open(path, "wb") as seap:  # Pickling
+            pickle.dump(list_to_upload, seap)
 
 
 if __name__ == "__main__":
-    search_words = ['student', 'software']
-    group_id = '232940231122621'
-    number_of_page_to_harvest = 10
     emails_dict = harvest_emails_fb_group_posts(group_id=group_id, pages_to_harvest=number_of_page_to_harvest,
                                                 search_keys=search_words)
     sorted_emails_tup_list = sort_list_by_dict_counter(emails_dict)
     grade = len(search_words)
-    mail_content = open("C:\\Users\\erann\\Desktop\\Eran Nir\\My Projects\\small_projects\\mail_content").read()
-    mail_subject = 'Eran Nir - Resume'
-    mail_attach = "C:\\Users\\erann\\Desktop\\Eran Nir\\My Projects\\small_projects\\Eran Nir - Resume.pdf"
-    sent_email_addresses = send_resume_to_rec(sorted_emails_tup_list, grade, mail_subject, mail_content, mail_attach)
-    sent_email_addresses_path = 'C:\\Users\\erann\\Desktop\\Eran Nir\\My Projects\\small_projects\\sent_email_addresses.pkl'
-    upload_list_pickle(list_to_upload=sent_email_addresses, path=sent_email_addresses_path, upload=False)
+    mail_content = open(mail_content_path).read()
+    sent_email_addresses_list = send_resume_to_rec(sorted_emails_tup_list,
+                                                   grade, mail_subject, mail_content, mail_attach)
+    upload_list_pickle(list_to_upload=sent_email_addresses_list, path=sent_email_addresses_path, upload=False)
